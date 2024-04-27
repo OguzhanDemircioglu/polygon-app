d273345c97416f7bf43c13641843ada8d8915ee7
Oğuzhan Turgut <oguzhanturgut611@gmail.com>
2024-04-27 03:23:11 +0300

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,36 +10,40 @@ import Feature from 'ol/Feature.js';
 import Point from 'ol/geom/Point';
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
 import { MapBrowserEvent } from 'ol';
-import { Geometry } from 'ol/geom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   clickedPoints: Point[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    // Haritayı oluştur
+    
     const map = new Map({
       target: 'map',
       layers: [
         new TileLayer({
-          source: new OSM()
+          source: new OSM({
+            attributions: '',
+            crossOrigin: 'anonymous'            
+          })
         })
       ],
       view: new View({
-        center: [0, 0],
-        zoom: 2
-      })
+        center: [39.9392, 32.8962],
+        zoom: 1,
+      }),
     });
 
-    // Noktaları tutacak vektör katmanını oluştur
     const vectorSource = new VectorSource();
     const vectorLayer = new VectorLayer({
       source: vectorSource,
@@ -47,29 +51,28 @@ export class AppComponent implements OnInit {
         image: new CircleStyle({
           radius: 6,
           fill: new Fill({ color: 'red' }),
-          stroke: new Stroke({ color: 'white', width: 2 })
-        })
-      })
+          stroke: new Stroke({ color: 'white', width: 2 }),
+        }),
+      }),
     });
     map.addLayer(vectorLayer);
 
-    // Kullanıcının tıklamalarını dinleyen fonksiyon
-    const addPointOnClick: (event: MapBrowserEvent<MouseEvent>) => void = (event) => {
+    const addPointOnClick: (event: MapBrowserEvent<MouseEvent>) => void = (
+      event
+    ) => {
       const features = vectorSource.getFeatures();
 
-      // En fazla 5 nokta oluşturulabilir
       if (features.length < 5) {
         const coordinate = map.getEventCoordinate(event.originalEvent);
         const point = new Feature({
-          geometry: new Point(coordinate)
+          geometry: new Point(coordinate),
         });
-        //this.clickedPoints.push(null); // Add clicked point to array
+        
         vectorSource.addFeature(point);
-        console.log('Clicked point:', coordinate); // Log each clicked point
+        console.log('Clicked point:', coordinate); 
       }
     };
 
-    // Haritada tıklamaları dinle
     map.on('click', addPointOnClick);
   }
 }
